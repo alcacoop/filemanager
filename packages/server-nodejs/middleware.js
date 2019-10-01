17e2d2ecf9f09176ac4c9ee1c2153f5103ff6bcd
Francesco Valente <francesco83.valente@gmail.com>
2019-10-01 17:16:23 +0200

    FEATURE: use encodeFn and decodeFn passed via router configs

--- packages/server-nodejs/middleware.js
+++ packages/server-nodejs/middleware.js
@@ -1,11 +1,15 @@
-'use strict';
+'use strict'
 
-const router = require('./router');
-const logger = require('./logger');
+const router = require('./router')
+const logger = require('./logger')
+const { encode, decode } = require('./utils/id')
 
-module.exports = config => router({
-  fsRoot: config.fsRoot,
-  rootName: config.rootName,
-  readOnly: config.readOnly,
-  logger: config.logger || logger
-});
+module.exports = config =>
+  router({
+    fsRoot: config.fsRoot,
+    rootName: config.rootName,
+    readOnly: config.readOnly,
+    logger: config.logger || logger,
+    encodeFn: config.encodeFn || encode,
+    decodeFn: config.decodeFn || decode,
+  })
